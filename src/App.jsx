94daf2ced2e3218aa5b267d873006e77d335343a
Xephna <36911354+Xephna@users.noreply.github.com>
2023-03-17 23:00:27 +0800

    fix background video width issue, fix asset path

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from './assets/images/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div class="App">
-    <div className="vid-container">
-      <video class="bg-vid" autoPlay muted loop>
-        <source src="../public/vessel.mp4" type="video/mp4"/>
-      </video>
-    </div>
-      <div class="container">
-        <h1>NO</h1>
+    <div className="App">
+      <div className="vid-container">
+        <video className="bg-vid" autoPlay muted loop>
+          <source src="./assets/videos/Vessel.mp4" type="video/mp4"/>
+        </video>
+      </div>
+      <div className="text-container">
+        <h1>NO</h1><br />
         <h2>Silksong is not out yet</h2>
       </div>
+      <div className="footer">PS: I love you Team Cherry take your time &lt;3</div>
     </div>
   )
 }
