d476e75483db6ed12c7a8a952801f3c6b1177057
Xephna <36911354+Xephna@users.noreply.github.com>
2023-03-17 17:40:31 +0800

    add test background

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,16 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div class="App">
+    <div className="vid-container">
+      <video class="bg-vid" autoPlay muted loop>
+        <source src="../public/vessel.mp4" type="video/mp4"/>
+      </video>
+    </div>
       <div class="container">
-            <h1>NO</h1>
-            <h2>Silksong is not out yet</h2>
-        </div>
+        <h1>NO</h1>
+        <h2>Silksong is not out yet</h2>
+      </div>
     </div>
   )
 }
